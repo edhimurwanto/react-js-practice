652f487286d6fba7914517b4440f6fc6dc7c4c6b
Edi Murwanto <uchihaedhi27@gmail.com>
2020-03-18 18:05:50 +0700

    enable styled-components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,44 +1,78 @@
 import React from 'react';
-import './App.css';
-import Header from '../header/Header';
-import Card from '../card/Card';
+import AppBarWithSearchPrimary from '../header/AppBarWithSearchPrimary';
+import Login from '../auth/Login';
+import { Modal, Fade, Backdrop } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
-class App extends React.Component {
-constructor(props){
-  super(props)
-  this.state={
-  person : [ {
-    name: "Edi",
-    age : 20
-  } ]
+const GlobalState = createGlobalStyle`{
+  body {
+    background-color: ${props => (props.theme.mode === 'dark' ? '#111' : '#EEE')};
+    color: ${props => (props.theme.mode === 'dark' ? '#EEE' : '#111')};
   }
-this.tambah=this.tambah.bind(this)
-}
+}`
 
-tambah(){
-  let someone = {name:"Like You", age:45}
-  this.state.person.push(someone)
-  this.setState({
-    person : this.state.person
-  })
-}
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    border: '2px solid #000',
+  },
+}));
 
-  render() {
+function App() {
 
-    let cards = [];
-    for (let index = 0; index < this.state.person.length; index++) {
-      cards.push(<Card key={index} persons={this.state.person[index]}/>);
-    }
-    
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [theme, setTheme] = React.useState({ mode: 'light' })
 
-    return (
-      <div className="App">
-        <Header />
-        {cards}
-        {/* <button id="btn1" onClick={this.tambah} >Add Card</button> */}
-      </div>
-    );
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChangeTheme = () => {
+    setTheme(theme.mode === 'dark' ? { mode: 'light' } : { mode: 'dark' });
   }
+
+  const handleSubmit = () => {
+    handleClose();
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalState />
+        <div className="App">
+          <AppBarWithSearchPrimary handleOpen={handleOpen} handleChangeTheme={handleChangeTheme} theme={theme} />
+          <Modal
+            aria-labelledby="login-form-modal"
+            aria-describedby="login-form-modal"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <Login handleSubmit={handleSubmit}/>
+              </div>
+            </Fade>
+          </Modal>
+        </div>
+      </>
+    </ThemeProvider>
+  )
 }
 
 export default App;
